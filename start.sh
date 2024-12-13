c9f12352e6e85d19f41584572262c51141977182
ᵛˣ ᵐᵒʰᵃᵐᵉᵈ <172046369+vx-moha@users.noreply.github.com>
2024-12-13 18:54:48 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ clear
 echo "Start Wine"
 termux-x11 :0 &>/dev/null &
 export DISPLAY=:0
-echo "Open Termux:X11
+echo "Open Termux:X11"
 ~/wine_hangover/arm64-v8a/bin/wine explorer /desktop=shell explorer
